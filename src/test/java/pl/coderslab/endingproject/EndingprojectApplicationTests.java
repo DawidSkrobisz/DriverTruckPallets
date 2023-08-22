51b3797eee4993ee65908537dc06c275c1df55cd
DawSkr <dawidskrobisz88@gmail.com>
2023-08-22 17:53:31 +0200

    endingProject

---
+++ src/test/java/pl/coderslab/endingproject/EndingprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.coderslab.endingproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EndingprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
