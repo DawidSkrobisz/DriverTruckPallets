be41fea6ffd7f9fc0e4aaf86146d80301dcbf5aa
DawSkr <dawidskrobisz88@gmail.com>
2023-10-21 13:39:24 +0200

    gitTest

--- src/main/java/pl/coderslab/endingproject/controller/HomeController.java
+++ src/main/java/pl/coderslab/endingproject/controller/HomeController.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.List;
 
 @Controller
 @AllArgsConstructor
