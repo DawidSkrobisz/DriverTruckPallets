ba830bf155392e331a8bee91da18ffd13eee71ef
DawSkr <dawidskrobisz88@gmail.com>
2023-10-21 10:38:48 +0200

    securityToDo

--- src/main/java/pl/coderslab/endingproject/EndingprojectApplication.java
+++ src/main/java/pl/coderslab/endingproject/EndingprojectApplication.java
@@ -2,10 +2,6 @@ package pl.coderslab.endingproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class EndingprojectApplication {
@@ -13,7 +9,4 @@ public class EndingprojectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EndingprojectApplication.class, args);
 	}
-
-
-
 }
