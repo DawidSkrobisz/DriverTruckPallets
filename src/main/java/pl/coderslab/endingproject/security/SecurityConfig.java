62f98d3cbfb3678a8ab0a601003f9354c73ad64d
DawSkr <dawidskrobisz88@gmail.com>
2023-10-21 13:15:04 +0200

    secNotWorking

--- src/main/java/pl/coderslab/endingproject/security/SecurityConfig.java
+++ src/main/java/pl/coderslab/endingproject/security/SecurityConfig.java
@@ -1,23 +1,27 @@
-/*
 package pl.coderslab.endingproject.security;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Bean
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/").permitAll()
-                .antMatchers("/about").authenticated()
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/public/**").permitAll()  // Dostęp publiczny
+                .antMatchers("/private/**").authenticated()  // Wymaga uwierzytelnienia
+                .antMatchers("/admin/**").hasRole("ADMIN")  // Wymaga roli ADMIN
                 .and()
-                .formLogin();
+                .formLogin()
+                .loginPage("/login")  // Strona logowania
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
     }
 }
-*/
